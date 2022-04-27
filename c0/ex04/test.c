747ce4b6efcc38c3870e29bf2f61dc062b477e23
daniil <d.uporov86@gmail.com>
2022-04-27 11:14:56 +0700

    ex04

---
+++ c0/ex04/test.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+void ft_putchar(char c){
+	write(1,&c,1);
+}
+
+void ft_is_negative(int n){
+	if (n>=0){
+		ft_putchar('P');
+	}
+	else{
+		ft_putchar('N');
+	}
+}
+
+int main(){
+	ft_is_negative(5);
+	ft_is_negative(0);
+	ft_is_negative(-1);
+	return 0;
+}
