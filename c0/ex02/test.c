d2413bc91a7045aa201e7436e3537537b106b128
daniil <d.uporov86@gmail.com>
2022-04-27 10:30:46 +0700

    ex02

---
+++ c0/ex02/test.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void ft_putchar(char c){
+	write(1,&c,1);
+}
+
+void ft_print_reverse_alphabet(void){
+	char letter;
+
+	letter='z';
+	while (letter>='a'){
+		ft_putchar(letter);
+		letter--;
+	}
+}
+
+int main(){
+	ft_print_reverse_alphabet();
+	return 0;
+}
