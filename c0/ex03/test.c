a914e727f2f762363debd0d0f32d47af9d23bb7e
daniil <d.uporov86@gmail.com>
2022-04-27 10:45:59 +0700

    ex03

---
+++ c0/ex03/test.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void ft_putchar(char c){
+	write(1,&c,1);
+}
+
+void ft_print_numbers(void){
+	char number;
+
+	number='0';
+	while(number<='9'){
+		ft_putchar(number);
+		number++;
+	}
+}
+
+int main(){
+	ft_print_numbers();
+	return 0;
+}         
